38a8f464989ac7e7c00a2b951e9e3ec59041d01d
Nanyonjo Lesley <101117234+lesley256@users.noreply.github.com>
2024-02-07 14:45:18 +0300

    Add files via upload

---
+++ login credentials/index.js
@@ -0,0 +1,15 @@
+//console.log("Lesley")
+
+//function to validate user credentials
+function validCrendentials(email,password){
+
+//conditional statement to check for valid and invalid credentials
+if(email==='nanyonjolesley@gmail.com' && password === '22/U/6669@2024'){
+    console.log(`User email is ${email},Login successful`);
+}else{
+    console.log('Incorrect user credentials');
+
+}
+}
+//these are the valid credentials(arguments)
+validCrendentials('nanyonjolesley@gmail.com','22/U/6669@2024');
